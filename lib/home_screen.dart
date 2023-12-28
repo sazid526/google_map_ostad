59bff6fbf3a86be53f139c4477e3f430b0a87bcb
sazid526 <arefin15-14526@diu.edu.bd>
2023-12-28 07:28:32 +0600

    done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:location/location.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -12,7 +12,18 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final Completer<GoogleMapController> _controller =
-  Completer<GoogleMapController>();
+      Completer<GoogleMapController>();
+  late GoogleMapController googleMapController;
+  Location location = Location();
+
+  Future<void> getCurrentLocation() async {
+    final LocationData locationData = await location.getLocation();
+
+    googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(
+            target: LatLng(locationData.latitude!, locationData.longitude!),
+        zoom: 17)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,11 +37,10 @@ class _HomeScreenState extends State<HomeScreen> {
             zoom: 17,
             bearing: 0,
             tilt: 5),
-
-        onLongPress: (LatLng Latling){
+        onLongPress: (LatLng Latling) {
           print("On Long Press on $Latling");
         },
-        onCameraMove: (cameraPosition){
+        onCameraMove: (cameraPosition) {
           print(cameraPosition);
         },
         zoomControlsEnabled: false,
@@ -40,82 +50,67 @@ class _HomeScreenState extends State<HomeScreen> {
         myLocationButtonEnabled: true,
         markers: {
           Marker(
-              markerId: MarkerId("Initial Position"),
+            markerId: MarkerId("Initial Position"),
             position: LatLng(23.427580326810077, 88.82347522459632),
             infoWindow: InfoWindow(
-              title: "This is my marker",
-              snippet: "This is my snippet"
-            ),
+                title: "This is my marker", snippet: "This is my snippet"),
             draggable: true,
-            onDrag: (LatLng position){
+            onDrag: (LatLng position) {
               print(position);
             },
-            onDragEnd: (LatLng position){
+            onDragEnd: (LatLng position) {
               print(position);
             },
-            onDragStart: (LatLng position){
+            onDragStart: (LatLng position) {
               print(position);
             },
-
-
           ),
           Marker(
             markerId: MarkerId("Initial Position"),
             position: LatLng(23.42868011524524, 88.82574263960123),
             infoWindow: InfoWindow(
-                title: "This is my marker",
-                snippet: "This is my snippet"
-            ),
+                title: "This is my marker", snippet: "This is my snippet"),
             draggable: true,
-            onDrag: (LatLng position){
+            onDrag: (LatLng position) {
               print(position);
             },
-            onDragEnd: (LatLng position){
+            onDragEnd: (LatLng position) {
               print(position);
             },
-            onDragStart: (LatLng position){
+            onDragStart: (LatLng position) {
               print(position);
             },
-
-
           ),
           Marker(
             markerId: MarkerId("Initial Position"),
             position: LatLng(23.430099840959297, 88.82391337305307),
             infoWindow: InfoWindow(
-                title: "This is my marker",
-                snippet: "This is my snippet"
-            ),
+                title: "This is my marker", snippet: "This is my snippet"),
             draggable: true,
-            onDrag: (LatLng position){
+            onDrag: (LatLng position) {
               print(position);
             },
-            onDragEnd: (LatLng position){
+            onDragEnd: (LatLng position) {
               print(position);
             },
-            onDragStart: (LatLng position){
+            onDragStart: (LatLng position) {
               print(position);
             },
-
-
           )
         },
         polylines: {
           Polyline(
               polylineId: PolylineId("This is polyline"),
-            color: Colors.red,
-            width: 6,
-            points: [
-              LatLng(23.427580326810077, 88.82347522459632),
-              LatLng(23.42868011524524, 88.82574263960123),
-              LatLng(23.430099840959297, 88.82391337305307),
-              LatLng(23.427580326810077, 88.82347522459632),
-            ]
-          )
+              color: Colors.red,
+              width: 6,
+              points: [
+                LatLng(23.427580326810077, 88.82347522459632),
+                LatLng(23.42868011524524, 88.82574263960123),
+                LatLng(23.430099840959297, 88.82391337305307),
+                LatLng(23.427580326810077, 88.82347522459632),
+              ])
         },
-
       ),
-
     );
   }
 }
--- lib/location.dart
+++ lib/location.dart
@@ -12,7 +12,27 @@ class LocationScreen extends StatefulWidget {
 
 class _LocationScreenState extends State<LocationScreen> {
   Location location = Location();
+  LocationData? currentLocation;
+  LocationData? myLocation;
+  late StreamSubscription locationSubscription;
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    listenToLocation();
+  }
+
+  void listenToLocation(){
+    locationSubscription = location.onLocationChanged.listen((locationData) {
+      myLocation = locationData;
+      if(mounted){
+        setState(() {
+
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +45,16 @@ class _LocationScreenState extends State<LocationScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('My location '),
+            Text('My location ${myLocation?.latitude ?? ""}, ${myLocation?.longitude ?? ""}'),
+            Text('Current location ${currentLocation?.latitude ?? ""}, ${currentLocation?.longitude ?? ""}'),
+
 
             ElevatedButton(
               onPressed: () async {
-               LocationData currentLocation = await location.getLocation();
+              currentLocation = await location.getLocation();
+              print(currentLocation?.latitude);
+              print(currentLocation?.longitude);
+              print(currentLocation?.altitude);
                 if (mounted) {
                   setState(() {});
                 }
@@ -40,6 +65,13 @@ class _LocationScreenState extends State<LocationScreen> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    locationSubscription.cancel();
+    super.dispose();
   }
 
 }
